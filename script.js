8cba5b1a4f790e0c4580b21392e0bc6318b2bb24
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-10-03 14:27:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // Your code here.
+ let isDragging = false;
+ let offsetX, offsetY;
+
+        document.querySelectorAll('.item').forEach(item => {
+            item.addEventListener('mousedown', startDragging);
+            item.addEventListener('mousemove', drag);
+            item.addEventListener('mouseup', stopDragging);
+        });
+
+        function startDragging(event) {
+            isDragging = true;
+            let rect = event.target.getBoundingClientRect();
+            offsetX = event.clientX - rect.left;
+            offsetY = event.clientY - rect.top;
+        }
+
+        function drag(event) {
+            if (isDragging) {
+                let x = event.clientX - offsetX;
+                let y = event.clientY - offsetY;
+                event.target.style.transform = `translate(${x}px, ${y}px)`;
+            }
+        }
+
+        function stopDragging() {
+            isDragging = false;
+        }
